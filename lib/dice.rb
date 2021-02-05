c7c043a7a6eb32530270eb38809ac1632bf7c2c7
joey hornsby <joey.s.hornsby@gmail.com>
2021-02-05 10:23:38 +0000

    added functionality to record dice s cores within Dice instance variable

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,8 +1,15 @@
 class Dice
+  attr_reader :tracked_scores
+
+  def initialize
+    @tracked_scores = []
+  end
+
   def roll(number = 1)
     scores = []
     number.times { scores.push(rand(1..6)) }
+    @tracked_scores << scores
     scores
-
   end
+
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -4,6 +4,10 @@ describe Dice do
     expect(subject).to respond_to(:roll).with(1).argument
   end
 
+#  it "tracks rolls" do
+#    expect(subject).to have_attribute(:tracked_scores)
+#  end
+
   it "when rolled returns a number between 1 and 6" do
     expect(subject.roll[0]).to be_between(1, 6)
   end
@@ -12,5 +16,11 @@ describe Dice do
     expect(subject.roll(4).length).to eq(4)
 
   end
+
+  it "expects tracked_rolls to return historical rolls" do
+    5.times { subject.roll }
+    expect(subject.tracked_scores.length).to eq 5
+  end
+
 
 end
